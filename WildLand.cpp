3dfae58ae1759e25fd6ebb4a4006d3c01b2f253a
stringzzz <119706411+stringzzz@users.noreply.github.com>
2022-12-08 16:55:01 -0800

    Update WildLand.cpp

--- WildLand.cpp
+++ WildLand.cpp
@@ -1,7 +1,7 @@
 /*
 
     Part of WildLand, an artificial organism environment gambling game, main part
-    Copyright (C) 2011 stringzzz, Ghostwarez Co.
+    Copyright (C) 2021 stringzzz, Ghostwarez Co.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
